6606a26f6e22b624869b46e0380564c8d0afee2e
atmabdullaham <atmabdullaham@gmail.com>
2025-01-10 12:28:27 +0600

    functions for show fetch video and show videos and show title dynamically

--- script/video.js
+++ script/video.js
@@ -9,8 +9,17 @@ const loadCategories = () => {
   .then((data) => displayCategories(data.categories))
   .catch((error) => console.log(error))
 }
+const loadVideos = () => {
+ // fetch the data
+ fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+  .then((res) => res.json())
+  .then((data) => displayVideos(data.videos))
+  .catch((error) => console.log(error))
+}
+
 
 loadCategories()
+loadVideos()
 
 // Create DisplayCategories
 const displayCategories = (categories) => {
@@ -26,6 +35,35 @@ const displayCategories = (categories) => {
   // add button to category container
 
   categoryContainer.appendChild(button)
+
+ });
+}
+
+
+
+// Display Videos
+const displayVideos = (videos) => {
+ console.log(videos)
+ const videoContainer = document.getElementById("videos")
+ videos.forEach(video => {
+  console.log(video)
+  const card = document.createElement("div");
+  card.classList = "card card-compact"
+  card.innerHTML = `
+  <figure>
+    <img
+      src=${video.thumbnail} />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title
+   }</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+  </div>
+  `
+
+  videoContainer.appendChild(card)
+
+
 
  });
 }
