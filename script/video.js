6c18c84603cef9265f1fe45345e99a8c31beeb15
atmabdullaham <atmabdullaham@gmail.com>
2025-01-11 07:25:45 +0600

    load and display videos based on category

--- script/video.js
+++ script/video.js
@@ -68,6 +68,8 @@ const loadCategories = () => {
   .then((data) => displayCategories(data.categories))
   .catch((error) => console.log(error))
 }
+
+
 const loadVideos = () => {
  // fetch the data
  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
@@ -75,10 +77,22 @@ const loadVideos = () => {
   .then((data) => displayVideos(data.videos))
   .catch((error) => console.log(error))
 }
+const loadCategoryVideo = (id) => {
+ fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id} `)
+  .then((res) => res.json())
+  .then((data) => displayVideos(data.category))
+  .catch((error) => console.log(error))
+
+}
+
 
 
 loadCategories()
 loadVideos()
+
+
+
+
 
 // Create DisplayCategories
 const displayCategories = (categories) => {
@@ -87,13 +101,18 @@ const displayCategories = (categories) => {
   console.log(item)
 
   // create a button
-  const button = document.createElement("button")
-  button.classList = "btn";
-  button.innerText = item.category;
+  const buttonContainer = document.createElement("div")
+  buttonContainer.innerHTML = `
+  <button onClick = "loadCategoryVideo(${item.category_id})" class = "btn" >
+  ${item.category}
+  </button>
+  `
+
+
 
   // add button to category container
 
-  categoryContainer.appendChild(button)
+  categoryContainer.appendChild(buttonContainer)
 
  });
 }
@@ -102,8 +121,9 @@ const displayCategories = (categories) => {
 
 // Display Videos
 const displayVideos = (videos) => {
- console.log(videos)
+
  const videoContainer = document.getElementById("videos")
+ videoContainer.innerHTML = "";
  videos.forEach(video => {
   console.log(video)
   const card = document.createElement("div");
@@ -112,7 +132,7 @@ const displayVideos = (videos) => {
   <figure class = "h-[200px] relative">
     <img class = "h-full w-full object-cover"
       src=${video.thumbnail} />
-      ${video.others.posted_date?.length == 0 ? "" : ` <span class = "absolute right-2 bottom-2 bg-black rounded p-1 text-white" > ${getTimeString(video.others.posted_date)}</span>`}
+      ${video.others.posted_date?.length == 0 ? "" : ` <span class = "absolute text-xs right-2 bottom-2 bg-black rounded p-1 text-white" > ${getTimeString(video.others.posted_date)}</span>`}
      
   </figure>
   <div class="px-0 py-2 flex gap-2">
